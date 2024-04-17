21a8374ae4892e88602e21f843104ee229238377
MuriloFalcMac <falcao.murilo4@gmail.com>
2024-04-17 16:22:29 -0300

    faz-se necessaria a conversao de dados

--- pd.py
+++ pd.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import auto
 from auto import regressao_quadratica
+from auto import regressao_linear
 from auto import main
 """
     nomePlanilha = '2024'
@@ -30,7 +31,35 @@ def main():
             x_quadratico = np.linspace(min(x), max(x), 100)
             y_quadratico = coefs[0]*x_quadratico**2 + coefs[1]*x_quadratico + coefs[2]
             plt.plot(x_quadratico, y_quadratico, color='red', label=equacao2)
-
+            
+    def linear():
+        # Realizar regressão linear
+        a, b = regressao_linear(x, y)
+        
+        # Equação da reta
+        equacao1 = f'f(x) = {a:f}x + {b:f}'
+        print("Equação da reta:", equacao1)
+        
+        # Plotar a reta
+        x_reta = np.linspace(min(x), max(x), 100)
+        y_reta = a * x_reta + b
+        plt.plot(x_reta, y_reta, color='green', label=equacao1)
+    
+    def getFunLin(x1):
+        a, b = regressao_linear(x, y)
+        y1 = a*x1 + b
+        print(y1)
+    
+    def getFunQua(x1):
+        a, b = regressao_quadratica(x, y)
+        y1 = a*x1**2 + b*x1 + c
+        print(y1)
+    
+    jan = 2567.20
+    
+    linear()
+    getFunLin(jan)
+    getFunQua(jan)
     quadratica()
     plt.xlabel('Precipitação acumulada no mês - Pará')
     plt.ylabel('Casos de dengue no estado')
